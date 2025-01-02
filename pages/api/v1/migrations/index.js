6015a2fa1fd06ac8732fbcea8edff545ebaf4e7d
Yvens Giacomini <31545881+superaura@users.noreply.github.com>
2025-01-02 02:40:08 +0000

    corrige o bug no endpoint de `migrations` restringindo métodos de requisição

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,8 +3,19 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
+
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+
+  let dbClient;
+
   try {
+    dbClient = await database.getNewClient();
+
     const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
@@ -31,9 +42,9 @@ export default async function migrations(request, response) {
       return response.status(200).json(migratedMigrations);
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    throw error;
   } finally {
     await dbClient.end();
-    return response.status(405).end();
   }
 }
